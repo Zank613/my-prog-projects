0a4970d3a7e2b0c5a9ff878300fd97da172bdbce
Ataerk YILDIRIM <114510382+Zank613@users.noreply.github.com>
2023-12-04 09:59:03 +0300

    Create functions.h

---
+++ beginner-challenges/function_based_simpCalculator/functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+double add(double a, double b);
+double substract(double a, double b);
+double multiply(double a, double b);
+double division(double a, double b);
+
+double getNumber();
+
+char getOp();
+
+#endif // FUNCTIONS_H_INCLUDED
